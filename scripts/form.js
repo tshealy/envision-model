2e40297af09ad849cd2536c7b1b7c81d3608e1ca
Joe VanLeeuwen <jvnlwn@gmail.com>
2014-03-31 10:09:10 -0400

    fixin wordcount bug

--- scripts/form.js
+++ scripts/form.js
@@ -7,13 +7,15 @@ function display(questions) {
 	// throw the questions into the DOM
 	displayQuestions(questions);
     // display scores
-    displayScores()
+    // displayScores()
     // display explanation text
     retrieveExplanations();
     // change selected and corresponding scores based on current session for value added
     updateSelect('va', 'valueAdded');
     // change selected based on current session for applicability
     updateSelect('ap', 'applicable');
+    // display scores
+    displayScores()
 	// need to re-update data ater updateSelect functions run
 	setSession();
 }
@@ -165,23 +167,36 @@ function retrieveExplanations() {
 	})
 }
 
+// // update selects with selected option selected
+// function updateSelect(klass, propName) {
+// 	$('.' + klass).each(function(index) {
+// 		var selectedIndex = envision.DOM[propName][index]
+// 		// check for 0 to avoid unecessary processing
+// 		if (selectedIndex) {
+// 			$(this).prop('selectedIndex', selectedIndex);
+//             // runs if applicable select is set to 'not applicable' so that its sibling valueAdded select will be disabled. Avoiding .change() because it affects too many other variables.
+//             if (klass === 'ap') {
+//                 var nextEl = $(this).parent().next();
+//                 // disable valueAdded select
+//                 nextEl.children().attr('disabled', 'disabled');
+//                 // replace score and possible points with '- -'
+//                 nextEl.next().text('- -').next().text('- -');
+//             }
+// 		}
+// 	})
+// }
+
 // update selects with selected option selected
 function updateSelect(klass, propName) {
-	$('.' + klass).each(function(index) {
-		var selectedIndex = envision.DOM[propName][index]
-		// check for 0 to avoid unecessary processing
-		if (selectedIndex) {
-			$(this).prop('selectedIndex', selectedIndex);
-            // runs if applicable select is set to 'not applicable' so that its sibling valueAdded select will be disabled. Avoiding .change() because it affects too many other variables.
-            if (klass === 'ap') {
-                var nextEl = $(this).parent().next();
-                // disable valueAdded select
-                nextEl.children().attr('disabled', 'disabled');
-                // replace score and possible points with '- -'
-                nextEl.next().text('- -').next().text('- -');
-            }
-		}
-	})
+    var maxScore = envision.maxScore
+    $('.' + klass).each(function(index) {
+        var selectedIndex = envision.DOM[propName][index]
+        // check for 0 to avoid unecessary processing
+        if (selectedIndex) {
+            $(this).prop('selectedIndex', selectedIndex).change();
+        }
+    })
+    console.log('maxScore: ', maxScore)
 }
 
 // character count
